983af5decc32c5bb38b98ee3c9be61d93619e5ce
laurac8r <itslaurakaminskiy@gmail.com>
2023-01-02 15:20:24 -0500

    made basic app

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  final game = MyGame();
+  runApp(GameWidget(game: game));
+}
+
+class MyGame extends FlameGame with HasCollisionDetection {
+  @override
+  Future<void> onLoad() async {
+    add(CenterComponent());
+
+    final sprite =
+        await Sprite.load('strawberry dress_smiling open eyes and blush.png');
+    final player = SpriteComponent(
+      sprite: sprite,
+      size: Vector2(100, 400),
+    );
+    add(player);
+  }
+
+  @override
+  Color backgroundColor() => const Color(0x00000000);
+}
+
+class CenterComponent extends PositionComponent {
+  CenterComponent()
+      : super(
+          position: Vector2(100, 100),
+          size: Vector2(50, 50),
+          anchor: Anchor.center,
+        );
+
+  @override
+  Future<void> onLoad() async {
+    await add(
+      PositionComponent(position: Vector2(0, -50)),
+    );
+  }
+}
